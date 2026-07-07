3ad4baff1b0d0c22dfa8c0d670c3e368ec4fc660
Richard Le <169632088+rlechuong@users.noreply.github.com>
2026-07-07 00:13:54 -0400

    Add session model with PrismaSessionStore

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,29 @@
 import "dotenv/config";
 import express from "express";
+import expressSession from "express-session";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import { prisma } from "./config/prisma.js";
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
+
+const secret = process.env.SESSION_SECRET;
+if (!secret) {
+  throw new Error("SESSION_SECRET environment variable not set.");
+}
+
+app.use(
+  expressSession({
+    cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 },
+    secret: secret,
+    resave: false,
+    saveUninitialized: false,
+    store: new PrismaSessionStore(prisma, {
+      checkPeriod: 2 * 60 * 1000,
+      dbRecordIdIsSessionId: true,
+    }),
+  }),
+);
 
 app.get("/", (req, res) => {
   res.send("File Uploader is running");
