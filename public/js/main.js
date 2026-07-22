e3a30ae3f898d40a87162ae56c647c6aa853e46b
Richard Le <169632088+rlechuong@users.noreply.github.com>
2026-07-22 01:09:52 -0400

    Add copy to clipboard for share links

--- public/js/main.js
+++ public/js/main.js
@@ -13,4 +13,19 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll("dialog[data-open='true']").forEach((dialog) => {
     dialog.showModal();
   });
+
+  const copyButton = document.querySelector("#copyShareLink");
+
+  copyButton?.addEventListener("click", async () => {
+    const input = document.querySelector("#shareLinkUrl");
+    if (!input) {
+      return;
+    }
+    const fullUrl = window.location.origin + input.value;
+    await navigator.clipboard.writeText(fullUrl);
+    copyButton.textContent = "Copied!";
+    setTimeout(() => {
+      copyButton.textContent = "Copy Link";
+    }, 2000);
+  });
 });
